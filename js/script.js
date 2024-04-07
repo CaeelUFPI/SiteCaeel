d0c03998993b82d6fb54bad218e7379b96e21d0b
CaeelUFPI <166236869+CaeelUFPI@users.noreply.github.com>
2024-04-07 11:43:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ window.onload = function(){
     load();
 }
 
-
 function load(){
     var menuBtn = document.getElementById("menu-btn");
     var navLinks = document.getElementById("nav-links");
@@ -15,3 +14,24 @@ function load(){
 
 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const photos = document.querySelectorAll(".photo");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  let currentIndex = 0;
+
+  prevButton.addEventListener("click", function() {
+    showPhoto(currentIndex - 1);
+  });
+
+  nextButton.addEventListener("click", function() {
+    showPhoto(currentIndex + 1);
+  });
+
+  function showPhoto(index) {
+    photos[currentIndex].classList.remove("active");
+    currentIndex = (index + photos.length) % photos.length;
+    photos[currentIndex].classList.add("active");
+  }
+});
