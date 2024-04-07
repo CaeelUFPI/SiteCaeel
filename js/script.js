8a099cde158adc382e90a75d033a784049fb7622
CaeelUFPI <166236869+CaeelUFPI@users.noreply.github.com>
2024-04-07 12:24:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,17 +21,25 @@ document.addEventListener("DOMContentLoaded", function() {
   const nextButton = document.querySelector(".next");
   let currentIndex = 0;
 
+  // Inicialmente, mostra apenas a primeira foto
+  photos[currentIndex].style.display = "block";
+
   prevButton.addEventListener("click", function() {
-    showPhoto(currentIndex - 1);
+    // Verifica se não é a primeira foto antes de mostrar a anterior
+    if (currentIndex > 0) {
+      photos[currentIndex].style.display = "none";
+      currentIndex--;
+      photos[currentIndex].style.display = "block";
+    }
   });
 
   nextButton.addEventListener("click", function() {
-    showPhoto(currentIndex + 1);
+    // Verifica se não é a última foto antes de mostrar a próxima
+    if (currentIndex < photos.length - 1) {
+      photos[currentIndex].style.display = "none";
+      currentIndex++;
+      photos[currentIndex].style.display = "block";
+    }
   });
-
-  function showPhoto(index) {
-    photos[currentIndex].classList.remove("active");
-    currentIndex = (index + photos.length) % photos.length;
-    photos[currentIndex].classList.add("active");
-  }
 });
+
